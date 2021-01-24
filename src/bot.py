df55980280af1492350d9f3a9ba90bd34bf8a943
= <junruihu@gmail.com>
2021-01-24 14:30:13 -0500

    message response logic

--- src/bot.py
+++ src/bot.py
@@ -14,17 +14,44 @@
 #           f"with access to: \n {' & '.join(str(g) for g in bot.guilds)}{HR}")
 
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
+GUILD = "memebot's hideout"  # todo: place with .env for prod
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    guild = discord.utils.get(client.guilds, name=GUILD)
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_message(message):
+    # if message.author == client.user:
+    #     return
+    print(message.author)
+
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
 
 client.run(TOKEN)
