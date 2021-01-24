cfe09a9a88155d045799aef8fe7edda1424a72d0
= <junruihu@gmail.com>
2021-01-24 14:44:47 -0500

    add commands

--- src/bot.py
+++ src/bot.py
@@ -17,11 +17,14 @@ import os
 import random
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = "memebot's hideout"  # todo: place with .env for prod
+bot = commands.Bot(command_prefix='!')
+
 client = discord.Client()
 
 @client.event
@@ -34,13 +37,37 @@ async def on_ready():
         f'{guild.name}(id: {guild.id})'
     )
 
-@client.event
-async def on_message(message):
-    # if message.author == client.user:
-    #     return
-    print(message.author)
+# @client.event
+# async def on_message(message):
+#     # if message.author == client.user:
+#     #     return
+#     print(message.author)
+#
+#
+#     brooklyn_99_quotes = [
+#         'I\'m the human form of the 💯 emoji.',
+#         'Bingpot!',
+#         (
+#             'Cool. Cool cool cool cool cool cool cool, '
+#             'no doubt no doubt no doubt no doubt.'
+#         ),
+#     ]
+#
+#     if message.content == '99!':
+#         response = random.choice(brooklyn_99_quotes)
+#         await message.channel.send(response)
 
 
+@bot.command(name='roll_dice', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice, number_of_sides):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
+@bot.command(name='99')
+async def nine_nine(ctx):
     brooklyn_99_quotes = [
         'I\'m the human form of the 💯 emoji.',
         'Bingpot!',
@@ -50,8 +77,7 @@ async def on_message(message):
         ),
     ]
 
-    if message.content == '99!':
-        response = random.choice(brooklyn_99_quotes)
-        await message.channel.send(response)
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
 
 client.run(TOKEN)
